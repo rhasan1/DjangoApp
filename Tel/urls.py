eec0757af822a567b4cf9068486076d473711dd6
Arif2021 <rakibul.arif@wool.com>
2021-08-28 21:12:58 +1000

    Login and User Authentication

--- Tel/urls.py
+++ Tel/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
 
     path('',include('travello.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
